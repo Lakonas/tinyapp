4c3b427262c1868ecb4f81c69786397d6bba61a0
Bill Katsoulis <bill.katsoulis@rogers.com>
2024-11-27 22:40:05 -0500

    Updated

--- express_server.js
+++ express_server.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString(length = 6) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
 
 
 app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -20,6 +29,7 @@ app.listen(PORT, () => {
 
   
 });
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -34,8 +44,14 @@ app.get("/urls", (req, res) => {
 });
 
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
-
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls/:id", (req, res) => {
   console.log(req.params,"req.params")
